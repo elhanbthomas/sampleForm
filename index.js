02c13a56145e299c70ff58582e95f260e8d122f0
Elhan B Thomas <147646623+elhanbthomas@users.noreply.github.com>
2025-03-19 22:38:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+let date = new Date();
+let year = date.getFullYear();
+let month = String(date.getMonth() + 1).padStart(2, '0');
+let day = String(date.getDate()).padStart(2, '0');
+
+let maxDate = (year - 18) + "-" + month + "-" + day;
+let minDate = (year - 55) + "-" + month + "-" + day;
+
+console.log("Max Date:", maxDate);
+console.log("Min Date:", minDate);
+
+const DOB = document.getElementById("dob");
+if (DOB) {
+    DOB.setAttribute("max", maxDate);
+    DOB.setAttribute("min", minDate);
+}
+
+let form = document.getElementById("form");
+form.addEventListener("submit", addData);
+
+});
+
+
+const fetchData = () => {
+    const data = localStorage.getItem("UserData");
+    const parsed = JSON.parse(data);
+    let table_values = "";
+    const tbody = document.getElementById("tbody");
+    parsed.forEach((row) => {
+        let row_data = "<tr><td>" + row.name + "</td><td>" + row.email + "</td><td>" + 
+        row.password + "</td><td>" + row.dob + "</td><td>" + row.terms + "</td></tr>";
+        table_values += row_data;
+    });
+    console.log(table_values);
+    tbody.innerHTML = table_values;
+}
+
+let entries = [];
+const addData = (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const terms = document.getElementById("terms").checked;
+
+    const data = {
+        name: name,
+        email: email,
+        password: password,
+        dob: dob,
+        terms: terms,
+    };
+    entries.push(data);
+
+    localStorage.setItem("UserData", JSON.stringify(entries));
+    form.reset();
+
+    fetchData();
+}
